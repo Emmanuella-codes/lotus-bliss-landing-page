ea0b7870caf72160a2cf9d468e6e6ea220b13bac
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-06-15 19:27:33 +0100

    test-mode-- navbar and hero with undecided bg color

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <div class="wrapper mx-auto max-w-7xl"></div>
+  <div
+    class="wrapper mx-auto max-w-xl lg:max-w-7xl my-24 lg:my-32 px-2 lg:px-8 flex lg:flex-row flex-col-reverse gap-14 lg:gap-24"
+  >
+    <div class="w-full">
+      <img class="" src="../assets/images/hero-img.jpg" alt="hero-img" />
+    </div>
+    <div class="w-full">
+      <h1 class="text-6xl lg:text-8xl">
+        Feel the <span class="text-teal-200">Lightness</span> of your
+        <span class="text-teal-300">body</span>
+      </h1>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style scoped></style>
