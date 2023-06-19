beb7093c11c4c8ebf85e20fad6f3add92e3a5e25
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-06-19 23:06:08 +0100

    fix image carousel

--- src/components/OurServices.vue
+++ src/components/OurServices.vue
@@ -1,154 +1,52 @@
-<script setup></script>
+<script>
+import { defineComponent } from 'vue'
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/splide/css/sea-green'
+import '@splidejs/vue-splide/css/core'
+
+export default defineComponent({
+  components: {
+    Splide,
+    SplideSlide
+  },
+
+  setup() {
+    const options = {
+      autoplay: true
+    }
+    return { options }
+  }
+})
+</script>
 
 <template>
   <div class="mx-auto max-w-[85%] lg:max-w-7xl flex justify-between items-center flex-col">
     <div class="w-full text-center">
       <h2 class="text-4xl lg:text-5xl font-bold">Our Services</h2>
     </div>
-    <!--carousel-->
-    <div id="default-carousel" class="relative w-full lg:w-3/5 my-7" data-carousel="slide">
+    <!-- Carousel -->
+    <div class="w-full lg:w-3/5 my-7">
       <!-- Carousel wrapper -->
-      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <!-- Item 1 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-1.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-2.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        <!-- Item 3 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-3.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        <!-- Item 4 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-4.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        <!-- Item 5 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-5.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        <!-- Item 6 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="../assets/images/yoga-6.jpg"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-      </div>
-      <!-- Slider indicators -->
-      <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
-      </div>
-      <!-- Slider controls -->
-      <button
-        type="button"
-        class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-      >
-        <span
-          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 19l-7-7 7-7"
-            ></path>
-          </svg>
-          <span class="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next
-      >
-        <span
-          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M9 5l7 7-7 7"
-            ></path>
-          </svg>
-          <span class="sr-only">Next</span>
-        </span>
-      </button>
+      <Splide :options="options">
+        <SplideSlide>
+          <img src="../assets/images/yoga-1.jpg" alt="Slide 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="../assets/images/yoga-2.jpg" alt="Slide 2" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="../assets/images/yoga-3.jpg" alt="Slide 3" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="../assets/images/yoga-4.jpg" alt="Slide 4" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="../assets/images/yoga-5.jpg" alt="Slide 5" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="../assets/images/yoga-6.jpg" alt="Slide 6" />
+        </SplideSlide>
+      </Splide>
     </div>
   </div>
 </template>
