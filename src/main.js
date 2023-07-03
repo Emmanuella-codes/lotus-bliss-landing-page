7d5966dfcb700db7545810457f485ba4640a2b57
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-07-03 14:29:35 +0100

    fix build error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/base.css'
 import './index.css'
 import 'flowbite'
 import VueSplide from '@splidejs/vue-splide'
